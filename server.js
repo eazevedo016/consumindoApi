277bd647dd097406e25725421f42787a31f8257f
Eduardo Azevedo dos Santos <05120106170@seguranca.local>
2022-02-02 15:36:23 -0400

    consumindo no front

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
+const cors = require('cors')
 const express = require('express')
 const app = express()
 
+app.use(cors())
+
 
 app.get('/', (req,res) => {
-    return res.json({message: 'ok'})
+    return res.json({message: 'okasdsa'})
 })
 
-app.listen('4679')
+
+app.listen('4679') 
